85fcc4168862ca322e38886db0c2cde10b58f45b
tiennv0608 <tiennv0608@gmail.com>
2021-08-27 10:22:08 +0700

    Xong xóa

--- src/app/component/player-delete/player-delete.component.ts
+++ src/app/component/player-delete/player-delete.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Player} from '../../model/player';
+import {FormControl, FormGroup} from '@angular/forms';
+import {PlayerService} from '../../service/player.service';
+import {ActivatedRoute, ParamMap, Router} from '@angular/router';
 
 @Component({
   selector: 'app-player-delete',
@@ -6,10 +10,43 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./player-delete.component.css']
 })
 export class PlayerDeleteComponent implements OnInit {
+  player: Player = {};
+  playerForm: FormGroup = new FormGroup({
+    name: new FormControl(),
+    champ: new FormControl(),
+    kda: new FormControl(),
+    des: new FormControl(),
+  });
+  id?: any;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(private playerService: PlayerService,
+              private activatedRoute: ActivatedRoute,
+              private router: Router) {
   }
 
+  ngOnInit(): void {
+    this.activatedRoute.paramMap.subscribe((paramMap: ParamMap) => {
+      // @ts-ignore
+      this.id = +paramMap.get('id');
+      this.findById(this.id);
+    });
+  }
+
+  findById(id: number) {
+    return this.playerService.findById(id).subscribe(data => {
+      this.playerForm = new FormGroup({
+        name: new FormControl(data.name),
+        champ: new FormControl(data.champ),
+        kda: new FormControl(data.kda),
+        des: new FormControl(data.des),
+      });
+    });
+  }
+
+  delete(id: number) {
+    this.playerService.delete(id).subscribe(() => {
+      alert('Xóa thành công');
+      this.router.navigate(['players']);
+    });
+  }
 }
