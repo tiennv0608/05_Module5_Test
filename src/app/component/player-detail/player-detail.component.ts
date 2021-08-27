cfefde000049c8379f951c96cfcc439867a18196
tiennv0608 <tiennv0608@gmail.com>
2021-08-27 10:15:23 +0700

    xong cập nhật và detail

--- src/app/component/player-detail/player-detail.component.ts
+++ src/app/component/player-detail/player-detail.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Player} from '../../model/player';
+import {PlayerService} from '../../service/player.service';
+import {ActivatedRoute, ParamMap} from '@angular/router';
 
 @Component({
   selector: 'app-player-detail',
@@ -6,10 +9,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./player-detail.component.css']
 })
 export class PlayerDetailComponent implements OnInit {
+  player: Player = {};
+  id?: number;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(private playerService: PlayerService,
+              private activatedRoute: ActivatedRoute) {
   }
 
+  ngOnInit(): void {
+    this.activatedRoute.paramMap.subscribe((paramMap: ParamMap) => {
+      // @ts-ignore
+      this.id = +paramMap.get('id');
+      this.findById(this.id);
+    });
+  }
+
+  findById(id: number) {
+    return this.playerService.findById(id).subscribe(data => {
+      this.player = data;
+    });
+  }
 }
--- src/app/component/player-edit/player-edit.component.ts
+++ src/app/component/player-edit/player-edit.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Player} from '../../model/player';
+import {FormControl, FormGroup} from '@angular/forms';
+import {PlayerService} from '../../service/player.service';
+import {ActivatedRoute, ParamMap, Router} from '@angular/router';
 
 @Component({
   selector: 'app-player-edit',
@@ -6,10 +10,46 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./player-edit.component.css']
 })
 export class PlayerEditComponent implements OnInit {
+  player: Player = {};
+  playerForm: FormGroup = new FormGroup({
+    name: new FormControl(),
+    champ: new FormControl(),
+    kda: new FormControl(),
+    des: new FormControl()
+  });
+  id?: any;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(private playerService: PlayerService,
+              private activatedRoute: ActivatedRoute,
+              private router: Router) {
   }
 
+  ngOnInit(): void {
+    this.activatedRoute.paramMap.subscribe((paramMap: ParamMap) => {
+      // @ts-ignore
+      this.id = +paramMap.get('id');
+      this.findById(this.id);
+    });
+  }
+
+  findById(id: number) {
+    return this.playerService.findById(id).subscribe(data => {
+      this.player = data;
+      this.playerForm = new FormGroup({
+        name: new FormControl(data.name),
+        champ: new FormControl(data.champ),
+        kda: new FormControl(data.kda),
+        des: new FormControl(data.des),
+      });
+    });
+  }
+
+  update(id: number) {
+    this.player = this.playerForm.value;
+    this.playerService.update(id, this.player).subscribe(() => {
+      alert('Cập nhật thành công');
+      this.router.navigate(['players']);
+    });
+
+  }
 }
